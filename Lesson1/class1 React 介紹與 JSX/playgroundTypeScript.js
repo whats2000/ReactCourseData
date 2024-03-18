8c99755451f0583130a3021227896635657faef3
whats2000 <whats2000mc@gmail.com>
2024-03-18 16:23:27 +0800

    修改: 第二周課程

--- Lesson1/class1 React 介紹與 JSX/playgroundTypeScript.js
+++
@@ -1,6 +0,0 @@
-var n = "John Doe";
-var age = 30;
-var greet = function (name, age) {
-    return "Hello, my name is ".concat(name, " and I am ").concat(age, " years old.");
-};
-console.log(greet(n, age)); // Hello, my name is John Doe and I am 30 years old.
--- Lesson1/class1 React 介紹與 JSX/playgroundTypeScript.ts
+++
@@ -1,8 +0,0 @@
-let n = "John Doe";
-let age = 30;
-
-const greet = (name: string, age: number): string => {
-    return `Hello, my name is ${name} and I am ${age} years old.`;
-};
-
-console.log(greet(n, age)); // Hello, my name is John Doe and I am 30 years old.
